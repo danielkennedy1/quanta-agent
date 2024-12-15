be36ee33ff5858776c1f780e4b89c9740cb9d571
Daniel Kennedy <danielrkennedy03@gmail.com>
2024-12-15 19:55:12 +0000

    Successful device -> agent -> ser

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import logging
+from time import sleep
 
 from quanta_client.models.device import Device
 from quanta_client.configuration import Configuration
 from quanta_client.api_client import ApiClient
 from quanta_client.api.default_api import DefaultApi
 from quanta_client.exceptions import ApiException
+from quanta_client.models.message import Message
+from quanta_client.models.metric import Metric
 
 from device.heater import Heater
 from config.config import Config
@@ -14,33 +17,85 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 root_logger = logging.getLogger()
 
+def register_to_server():
+    # Register the device and metrics
+    try:
+        device = api.device_create(Device(description="ESP32 Heater"))
+        logger.info(f"Device created: {device}")
+
+        if device.id is None:
+            logger.error("Device ID is None")
+            exit(1)
+
+        heater_id = device.id
+
+        temperature = api.metric_create(Metric(name="Temperature", data_type="float"))
+
+        if temperature.id is None:
+            logger.error("Temperature ID is None")
+            exit(1)
+
+        temperature_id = temperature.id
+
+        uptime = api.metric_create(Metric(name="Uptime", data_type="float"))
+
+        if uptime.id is None:
+            logger.error("Uptime ID is None")
+            exit(1)
+
+        uptime_id = uptime.id
+        
+        return heater_id, temperature_id, uptime_id
+    except ApiException as e:
+        logger.error(f"Exception when calling DefaultApi->device_create: {e}")
+        exit(1)
+
+
 if __name__ == "__main__":
     config = Config("config/config.json")
     config.logging.configure_logger(root_logger)
 
     heater = Heater(config.devices.device_list[0].ip, config.devices.device_list[0].port)
 
-    logger.info(f"Base URL: {config.server.base_url}")
-
     quanta_config = Configuration(host=config.server.base_url)
     client = ApiClient(quanta_config)
     api = DefaultApi(client)
 
-    # Create a device
-    device = Device.from_dict({"description": "Heater"})
+    heater_id, temperature_id, uptime_id = register_to_server()
 
-    if device is None:
-        logger.error("Device is None")
-        exit(1)
+    while True:
+        try:
+            temperature_result = heater.get_avg_temperature_for_minute()
+            uptime_result = heater.get_uptime_for_minute()
 
-    logger.info(f"Device: {device}")
+            if temperature_result is None or uptime_result is None:
+                logger.error("No response received from ESP32")
+                continue
 
-    try:
-        created_device = api.device_create(device)
+            temperature, temperature_timestamp = temperature_result
+            uptime, uptime_timestamp = uptime_result
 
-        logger.info(f"Created device: {created_device}")
-    except ApiException as e:
-        logger.error(f"Exception when calling DefaultApi->create_device: {e}")
+            logger.info(f"Temperature: {temperature} at {temperature_timestamp}")
+            logger.info(f"Uptime: {uptime} at {uptime_timestamp}")
+
+            logger.info("Sending data to server")
+            temperature_message = Message(device_id=heater_id, metric_id=temperature_id, metric_value=str(temperature), timestamp=temperature_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ'))
+            uptime_message = Message(device_id=heater_id, metric_id=uptime_id, metric_value=str(uptime), timestamp=uptime_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ'))
+
+            api.message_create(temperature_message)
+            api.message_create(uptime_message)
+
+            logger.info("Data sent to server")
+
+
+            logger.info("Sleeping..")
+            sleep(5)
+        except ApiException as e:
+            logger.error(f"Exception when calling DefaultApi->message_create: {e}")
+
+
+
+
 
 
     #logger.warning(heater.get_avg_temperature_for_minute())
